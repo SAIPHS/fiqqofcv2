5754508b06646ab8c57c6c41e1c293f38657f14d
DevFiqq <54147619+SAIPHS@users.noreply.github.com>
2021-11-28 15:19:29 +0800

    Initial commit

---
+++ commado/donasi.js
@@ -0,0 +1,15 @@
+const donasi = (x) => {
+      return `
+\`\`\`「 DONASI 」
+
+Pulsa:
+601151102125 (Fiqq)
+60199782326 (Adyy)
+
+Saweria:
+https://saweria.co/fiqqofc
+saweria.co/itsukichan\`\`\`
+`
+}
+
+exports.donasi = donasi
